140351cf87a80097786d1b26dc5a12e7056201ba
peerawitp <peerawitp@protonmail.com>
2026-02-02 19:17:56 +0700

    feat: update storage to use just one bucket

--- src/core/storage/storage.controller.ts
+++ src/core/storage/storage.controller.ts
@@ -2,15 +2,17 @@ import { config } from "../config";
 import { StorageService } from "./storage.service";
 
 export class StorageController {
-  constructor(private service: StorageService) {}
+  constructor(private service: StorageService) { }
 
   async uploadFile(params: { bucket: string; key: string; file: File }) {
     const buffer = Buffer.from(await params.file.arrayBuffer());
-    return await this.service.uploadObject({
-      ...params,
+    await this.service.uploadObject({
+      bucket: config.s3.bucket!,
+      key: `${params.bucket}/${params.key}`,
       body: buffer,
       contentType: params.file.type,
     });
+    return params.key;
   }
 
   async getPresignedFileUrl(params: {
@@ -18,14 +20,21 @@ export class StorageController {
     key: string;
     expiresIn?: number;
   }) {
-    return await this.service.presignGetObject(params);
+    return await this.service.presignGetObject({
+      bucket: config.s3.bucket!,
+      key: `${params.bucket}/${params.key}`,
+      expiresIn: params.expiresIn,
+    });
   }
 
   getFileUrl(params: { bucket: string; key: string }) {
-    return `${config.s3.endpoint}/${params.bucket}/${params.key}`;
+    return `${config.s3.endpoint}/${config.s3.bucket!}/${params.bucket}/${params.key}`;
   }
 
   async deleteFile(params: { bucket: string; key: string }) {
-    return await this.service.deleteObject(params);
+    return await this.service.deleteObject({
+      bucket: config.s3.bucket!,
+      key: `${params.bucket}/${params.key}`,
+    });
   }
 }
